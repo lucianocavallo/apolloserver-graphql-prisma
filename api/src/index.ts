e02ceb2b9877c4ece7cead42db1be39f2cd76cba
luchx88 <lucianocavallo@gmail.com>
2022-02-10 17:35:34 -0300

    Scalars, filtros y queries avanzadas

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,20 +1,9 @@
 import { ApolloServer } from 'apollo-server';
+import path from 'path';
+import { readFileSync } from 'fs';
+import resolvers from './resolvers';
 
-// 1 - query
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`;
-
-// 2 - resolvers
-const resolvers = {
-  Query: {
-    info: () => `This is the API of Platzi Node GraphQL`,
-  },
-};
-
-// 3 - iniciar servidor
+const typeDefs = readFileSync(path.join(__dirname, 'schema.graphql'), 'utf-8');
 
 const server = new ApolloServer({
   typeDefs,
