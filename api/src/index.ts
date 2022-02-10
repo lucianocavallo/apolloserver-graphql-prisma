abe28d2f72365aa86317b4d3c99b74024f52cd5c
luchx88 <lucianocavallo@gmail.com>
2022-02-10 15:30:04 -0300

    Iniciando Proyecto con apollo-server

---
+++ api/src/index.ts
@@ -0,0 +1,24 @@
+import { ApolloServer } from 'apollo-server';
+
+// 1 - query
+const typeDefs = `
+  type Query {
+    info: String!
+  }
+`;
+
+// 2 - resolvers
+const resolvers = {
+  Query: {
+    info: () => `This is the API of Platzi Node GraphQL`,
+  },
+};
+
+// 3 - iniciar servidor
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => console.log(`Server is running on${url}`));
